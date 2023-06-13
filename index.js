fbcb2369d81d2eb60c0de1b458cf4c58e338b2db
ad.hanif <ad.hanif@outlook.com>
2023-06-13 23:43:18 +0200

    Question 34: solved

--- index.js
+++ index.js
@@ -578,10 +578,39 @@
 // ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
 // ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 
-function likes(names) {
-  return names.map((name)=>{
-    return 
-  })
-}
+// function likes(names) {
+//   if (names.length === 0) {
+//     return "no one likes this";
+//   } else if (names.length === 1) {
+//     return `${names[0]} likes this`;
+//   } else if (names.length === 2) {
+//     return `${names[0]} and ${names[1]} like this`;
+//   } else if (names.length === 3) {
+//     return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+//   } else if (names.length > 3) {
+//     return `${names[0]}, ${names[1]} and ${
+//       names.length - 2
+//     } others like this`;
+//   }
+// }
 
-console.log(likes(["Jacob", "Alex"]))
+// console.log(likes(["Alex", "Jacob", "Mark", "Max", "Jacob", "Alex", "Alex", "Jacob", "Mark", "Max", "Jacob", "Alex"]));
+
+// Question 34: Task: Your task is to write a function which returns the sum of following series upto nth term(parameter). Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+
+// Examples:(Input --> Output)
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+// function SeriesSum(n) {
+//   let a = 0;
+//   let x = 1;
+//   for (let i = 0; i < n; i++) {
+//     a += 1 / x;
+//     x = x + 3;
+//   }
+//   a = a.toFixed(2);
+//   return a;
+// }
+// console.log(SeriesSum(5));
