b252ca2bcf06bb710882fc80e3b3af95308af94e
ad.hanif <ad.hanif@outlook.com>
2023-08-15 23:12:54 +0200

    Question #10 solved

--- index.js
+++ index.js
@@ -1006,5 +1006,17 @@
 // const checkForFactor = (base, factor) => {
 //   return base % factor === 0 ? true : false;
 // };
-
 // console.log(checkForFactor(9, 2));
+
+// Question #51 Terminal game move function
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// Example:
+// move(3, 6) should equal 15
+
+// const move = (position, roll) => {
+//   return roll * 2 + position;
+// };
+// console.log(move(2, 5));
