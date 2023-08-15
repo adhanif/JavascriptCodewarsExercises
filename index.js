6cacdcb100959efb32c2f9f4241a4c5d9b3ee701
ad.hanif <ad.hanif@outlook.com>
2023-08-15 22:35:54 +0200

    Question #48 solved

--- index.js
+++ index.js
@@ -917,7 +917,7 @@
 
 // console.log(pokemons.name);
 
-// Q#46 We need a function that can transform a number (integer) into a string.
+// Question #46 We need a function that can transform a number (integer) into a string.
 
 // What ways of achieving this do you know?
 
@@ -932,7 +932,7 @@
 
 // console.log(numberToString(25));
 
-// Q#47  An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+// Question #47  An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
 // isIsogram "Dermatoglyphics" = true
 // isIsogram "moose" = false
 // isIsogram "aba" = false
@@ -948,3 +948,24 @@
 // }
 
 // console.log(isIsogram("aba"));
+
+// Question #48  You might know some pretty large perfect squares. But what about the NEXT one?
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+// Examples:(Input --> Output)
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1 since 114 is not a perfect square
+
+// const findNextSquare = (sq) => {
+//   const num = Math.sqrt(sq);
+//   if (num - Math.floor(num) !== 0) {
+//     return -1;
+//   } else {
+//     const newSqr = num + 1;
+//     return newSqr ** 2;
+//   }
+// };
+
+// console.log(findNextSquare(114));
