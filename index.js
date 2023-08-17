19a2e17b6391d30978758ed3222c7945ca04f6de
ad.hanif <ad.hanif@outlook.com>
2023-08-18 00:23:42 +0200

    Question #54 solved

--- index.js
+++ index.js
@@ -1062,3 +1062,17 @@
 // }
 
 // console.log(cockroachSpeed(1.08));
+
+// Question #54 In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+// Your function will be tested with pre-made examples as well as random ones.
+
+const findDifference = (a, b) => {
+ return Math.abs(
+    a.reduce((acc, curr) => acc * curr, 1) -
+      b.reduce((acc, curr) => acc * curr, 1)
+  );
+};
+console.log(findDifference([2, 2, 3], [5, 4, 1]));
