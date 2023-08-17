f0d13a83848cd1e1957e85b32711438dcc0b92d5
ad.hanif <ad.hanif@outlook.com>
2023-08-18 00:34:27 +0200

    Question #56 solved

--- index.js
+++ index.js
@@ -1089,3 +1089,14 @@
 //   return (Number(a) + Number(b)).toString();
 // }
 // console.log(sumStr("4", "5"));:
+
+// Question #56 Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+// function solution(str, ending) {
+//   return str.endsWith(ending);
+// }
+// console.log(solution("abcde", "cded"));
