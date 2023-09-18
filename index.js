4c6eba6518dc9be23d53281c9100d9044169c458
ad.hanif <ad.hanif@outlook.com>
2023-09-18 23:58:43 +0200

    Question 65 solved

--- index.js
+++ index.js
@@ -1443,3 +1443,17 @@
 // };
 
 // console.log(descendingOrder(42145));
+
+// Question #65 In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+// const filter_list = (x) => {
+//   return x.filter((ele) => {
+//     return typeof ele !== "string" ;
+//   });
+// };
+// console.log(filter_list([1, "a", "b", 0, 15]));
