7999dc7efad8294880ae27e66b58e70090f717ee
ad.hanif <ad.hanif@outlook.com>
2023-09-19 00:08:40 +0200

    Question 66 solved

--- index.js
+++ index.js
@@ -1457,3 +1457,16 @@
 //   });
 // };
 // console.log(filter_list([1, "a", "b", 0, 15]));
+
+// Question #66 Make a program that filters a list of strings and returns a list with only your friends name in it. If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+// friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+// Note: keep the original order of the names in the output.
+
+// const name1 = (arr) => {
+//   return arr.filter((ele) => {
+//     return ele.length === 4 ? ele : null;
+//   });
+// };
+
+// console.log(name1(["Ryan", "Kieran", "Mark"]));
