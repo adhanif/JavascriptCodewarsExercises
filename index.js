45488d8f6214d90a32b8bf3bacce36e5128e72a4
ad.hanif <ad.hanif@outlook.com>
2023-09-19 00:30:05 +0200

    Question 68 solved

--- index.js
+++ index.js
@@ -1485,3 +1485,19 @@
 // };
 
 // console.log(count("abcabc"));
+
+// Question #68  Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering. Write a function which takes a list of strings and returns each line prepended by the correct number. The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+// Examples: (Input --> Output)
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+// const number = (arr) => {
+//   if (arr === null) {
+//     return null;
+//   }
+//   return arr.map((ele, i) => {
+//     return `${String(i + 1)}: `.concat(ele);
+//   });
+// };
+
+// console.log(number(["a", "b", "c"]));
