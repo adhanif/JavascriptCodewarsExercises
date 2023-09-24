cc5db3b43847c0f5b120d3184d8345e104fe0299
ad.hanif <ad.hanif@outlook.com>
2023-09-24 22:50:17 +0200

    Question #73 completed

--- index.js
+++ index.js
@@ -1590,3 +1590,35 @@
 // }
 
 // findShort("Let's travel abroad shall we");
+
+// Question #73 given an array of integers, find the one that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+// function findOdd(A) {
+//   let total = {};
+//   A.forEach((ele) => {
+//     if (!total[ele]) {
+//       total[ele] = 1;
+//     } else {
+//       total[ele]++;
+//     }
+//   });
+
+//   let me = 0;
+//   for (let value in total) {
+//     if (total[value] % 2 != 0) {
+//       me = value;
+//     }
+//   }
+//   return Number(me);
+// }
+
+// // const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
+// console.log(findOdd([0]));
