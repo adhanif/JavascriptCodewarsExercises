aa5104b7ad70886e4dd41640d83b6600c2e072fc
ad.hanif <ad.hanif@outlook.com>
2023-09-26 00:02:41 +0200

    Question #75 completed

--- index.js
+++ index.js
@@ -1635,3 +1635,23 @@
 // }
 
 // console.log(removeExclamationMarks("Hello World!"));
+
+// Question #75 Given a string str, reverse it and omit all non-alphabetic characters.
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// function reverseLetter(str) {
+//   return str
+//     .split("")
+//     .map((ele) => {
+//       if (/[a-zA-Z]/.test(ele)) {
+//         return ele;
+//       }
+//     })
+//     .reverse()
+//     .join("");
+// }
+
+// console.log(reverseLetter("ab23c"));
