db965dd679221c844b18eaf64da665508dd6d2a3
ad.hanif <ad.hanif@outlook.com>
2023-09-26 00:54:21 +0200

    Question #78 completed

--- index.js
+++ index.js
@@ -1691,3 +1691,10 @@
 //     .join(" ");
 // }
 // console.log(order("is2 Thi1s T4est 3a"));
+
+// Question #78 You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false. You can assume all values in the array are numbers.
+
+// function smallEnough(a, limit) {
+//   return a.every((ele) => ele <= limit);
+// }
+// console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120));
