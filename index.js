3f417117b44275c630f75855d94bb547eea45273
ad.hanif <ad.hanif@outlook.com>
2023-09-26 00:15:32 +0200

    Question #76 completed

--- index.js
+++ index.js
@@ -1653,5 +1653,25 @@
 //     .reverse()
 //     .join("");
 // }
-
 // console.log(reverseLetter("ab23c"));
+
+// Question #76 Count the number of divisors of a positive integer n. Random tests go up to n = 500000.
+// Examples (input --> output)
+// 4 --> 3 // we have 3 divisors - 1, 2 and 4
+// 5 --> 2 // we have 2 divisors - 1 and 5
+// 12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+// 30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
+
+// function getDivisorsCnt(n) {
+//   let total = 0;
+//   for (let i = 1; i <= Math.sqrt(n); i++) {
+//     if (n % i === 0) {
+//       total++;
+//       if (i !== n / i) {
+//         total++;
+//       }
+//     }
+//   }
+//   return total;
+// }
+// console.log(getDivisorsCnt(500000));
