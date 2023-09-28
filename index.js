87999e3fe77776c49756a73343af3f3709ffbba0
ad.hanif <ad.hanif@outlook.com>
2023-09-28 22:17:34 +0200

    Question #82 solved

--- index.js
+++ index.js
@@ -1763,3 +1763,18 @@
 //   return marks < 0 ? 0 : marks;
 // }
 // console.log(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"]));
+
+// Question #82 Return the number (count) of vowels in the given string. We will consider a, e, i, o, u as vowels for this Kata (but not y). The input string will only consist of lower case letters and/or spaces.
+
+// return word.replace(/[aeiou]/gi, "");
+
+// function getCount(str) {
+//   let count = 0;
+//   str.split("").forEach((ele) => {
+//     if (/[aeiou]/.test(ele)) {
+//       count++;
+//     }
+//   });
+//   return count;
+// }
+// console.log(getCount("abracadabra"));
