53f1148a2fc634d4037bb357f3d0145ac9044e61
ad.hanif <ad.hanif@outlook.com>
2023-10-02 22:52:34 +0200

    Question #87 solved

--- index.js
+++ index.js
@@ -1847,3 +1847,16 @@
 //   return -1;
 // }
 // console.log(findEvenIndex([20, 10, -80, 10, 10, 15, 35]));
+
+// Question #87 Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+// None of the arrays will be empty, so you don't have to worry about that!
+
+// function removeEveryOther(arr) {
+//   return arr.filter((ele, i) => {
+//     return i % 2 === 0 ? ele : null;
+//   });
+// }
+// console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
