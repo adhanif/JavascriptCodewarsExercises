d4b2b1c8635862a1b958174b4bc6b9f97fc6139b
ad.hanif <ad.hanif@outlook.com>
2023-10-03 01:18:11 +0200

    Question #89 solved

--- index.js
+++ index.js
@@ -1891,3 +1891,35 @@
 //   }
 // }
 // console.log(bouncingBall(2, 0.5, 1));
+
+// Question #89: Your job is to write a function which increments a string, to create a new string.If the string already ends with a number, the number should be incremented by 1. If the string does not end with a number. the number 1 should be appended to the new string.
+// Examples
+// foo -> foo1
+// foobar23 -> foobar24
+// foo0042 -> foo0043
+// foo9 -> foo10
+// foo099 -> foo100
+// Attention: If the number has leading zeros the amount of digits should be considered.
+
+// function incrementString(str) {
+//   let arr = str.split(/([0-9]+)/).filter(Boolean);
+//   console.log(arr);
+//   if (/^\d+$/.test(arr)) {
+//     let number = parseInt(arr[0]);
+//     number++;
+//     arr[0] = number.toString().padStart(arr[0].length, "0");
+//     return arr.join("");
+//   } else if (/\d$/.test(str)) {
+//     let number = parseInt(arr[arr.length - 1]);
+//     console.log(arr[arr.length - 1]);
+//     number++;
+//     arr[arr.length - 1] = number
+//       .toString()
+//       .padStart(arr[arr.length - 1].length, "0");
+//     return arr.join("");
+//   } else if (!/\d$/.test(str)) {
+//     arr.push("1");
+//     return arr.join("");
+//   }
+// }
+// console.log(incrementString("fo99obar99"));
