1fd3172f269f6a6224d08a5246331102ab714fe2
ad.hanif <ad.hanif@outlook.com>
2023-10-04 22:46:26 +0200

    Question #91 solved

--- index.js
+++ index.js
@@ -1925,7 +1925,6 @@
 // console.log(incrementString("fo99obar99"));
 
 // Question #90: Complete the function which converts a binary number (given as a string) to a decimal number.
-
 // function binToDec(bin) {
 //   return bin
 //     .split("")
@@ -1935,3 +1934,29 @@
 //     }, 0);
 // }
 // console.log(binToDec("1001001"));
+
+// Question #91: In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number. Examples
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+// function highAndLow(numbers) {
+//   return numbers
+//     .split(" ")
+//     .map((ele) => Number(ele))
+//     .reduce(
+//       (result, curr) => {
+//         result[0] = Math.max(result[0], curr);
+//         result[1] = Math.min(result[1], curr);
+//         return result;
+//       },
+//       [-Infinity, Infinity]
+//     )
+//     .join(" ")
+//     .toString();
+// }
+// highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4");
