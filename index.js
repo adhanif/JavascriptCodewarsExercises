36e976a0b8a8b564ace09633c7a0c1304ec456d8
ad.hanif <ad.hanif@outlook.com>
2023-10-11 23:14:47 +0200

    Question #96 solved

--- index.js
+++ index.js
@@ -2038,3 +2038,27 @@
 //   return count;
 // }
 // console.log(nbDig(10, 1));
+
+// Question #96:Task Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list. Don't change the order of the elements that are left.
+
+// Examples
+// * Input: [1,2,3,4,5], output = [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+// function removeSmallest(num) {
+//   let min = Math.min(...num);
+//   let index = 0;
+//   for (let i = 0; i < num.length; i++) {
+//     if (num[i] === min) {
+//       index = i;
+//       break;
+//     }
+//   }
+//   return num.filter((n, i) => {
+//     if (i !== index) {
+//       return n;
+//     }
+//   });
+// }
+// console.log(removeSmallest([2, 2, 1, 2, 1]));
