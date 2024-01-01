6f7b319f76e8f359ff7fe1bd32ece0cd950c147c
ad.hanif <ad.hanif@outlook.com>
2024-01-01 20:56:38 +0100

    Question #112 solved

--- index.js
+++ index.js
@@ -2322,3 +2322,17 @@
 //   return binary.filter((ele) => ele === 1).length;
 // };
 // console.log(hammingWeight(1));
+
+// Question #112: Is every value in the array an array? This should only test the second array dimension of the array. The values of the nested arrays don't have to be arrays.
+// Examples:
+// [[1],[2]] => true
+// ['1','2'] => false
+// [{1:1},{2:2}] => false
+
+//Solution
+// const arrCheck = (value) => {
+//   return value.every((ele) => {
+//     return Array.isArray(ele) ? true : false
+//   })
+// }
+// console.log(arrCheck(['1', '2']))
