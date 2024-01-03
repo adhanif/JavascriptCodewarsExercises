1daadbc0532ce2874e81d0ae1379638b6cb5e4ad
ad.hanif <ad.hanif@outlook.com>
2024-01-03 20:32:38 +0100

    Question #114 solved

--- index.js
+++ index.js
@@ -2363,3 +2363,18 @@
 //     [2, 3],
 //   ]),
 // )
+
+// Question #114:Each floating-point number should be formatted that only the first two decimal places are returned. You don't need to check whether the input is a valid number because only valid numbers are used in the tests. Don't round the numbers! Just cut them after two decimal places!
+// Right examples:
+// 32.8493 is 32.84
+// 14.3286 is 14.32
+
+//Solution
+// function twoDecimalPlaces(number) {
+//   return Number(
+//     String(number).split('.')[0] +
+//       '.' +
+//       String(number).split('.')[1].slice(0, 2),
+//   )
+// }
+// console.log(twoDecimalPlaces(10.1289767789))
