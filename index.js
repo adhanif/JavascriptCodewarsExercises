88a846c45847e8d53e4ef51e160879dbdb2fe4b0
ad.hanif <ad.hanif@outlook.com>
2024-03-11 22:45:09 +0200

    Question #120 solved

--- index.js
+++ index.js
@@ -2476,3 +2476,37 @@
 // }
 
 // console.log(total([1, 2, 3, 4, 5, 6]));
+
+// function fizzBuzz(n) {
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('FizzBuzz');
+//     } else if (i % 3 === 0) {
+//       console.log('Fizz');
+//     } else if (i % 5 === 0) {
+//       console.log('Buzz');
+//     } else if (!(i % 3 === 0) && !(i % 5 === 0)) {
+//       console.log(i);
+//     }
+//   }
+// }
+// fizzBuzz(25);
+
+// Question #120: Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition. The binary number returned should be a string. Examples:(Input1, Input2 --> Output (explanation)))
+
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+//solution:
+
+// function addBinary(a, b) {
+//   let sum = a + b;
+//   let binary = '';
+//   while (sum > 0) {
+//     binary += sum % 2;
+//     sum = Math.floor(sum / 2);
+//   }
+//   return binary.split('').reverse().join('');
+// }
+
+// console.log(addBinary(13, 1));
