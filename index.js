065f7a74acd90f4280a5cc7c4a268abdf8de1ec0
ad.hanif <ad.hanif@outlook.com>
2024-09-04 23:45:22 +0300

    Question #125 solved

--- index.js
+++ index.js
@@ -2583,25 +2583,43 @@
 // "Success"  =>  ")())())"
 // "(( @"     =>  "))(("
 
-//colution
+//solution
 
-function duplicateEncode(word) {
-  let alpha = {};
-  let newString = '';
-  word = word.toLowerCase();
-  for (let char of word) {
-    alpha[char] = (alpha[char] || 0) + 1;
-  }
+// function duplicateEncode(word) {
+//   let alpha = {};
+//   let newString = '';
+//   word = word.toLowerCase();
+//   for (let char of word) {
+//     alpha[char] = (alpha[char] || 0) + 1;
+//   }
 
-  word.split('').forEach((ele) => {
-    if (alpha[ele] === 1) {
-      newString = newString.concat('(');
-    } else {
-      newString = newString.concat(')');
-    }
-  });
+//   word.split('').forEach((ele) => {
+//     if (alpha[ele] === 1) {
+//       newString = newString.concat('(');
+//     } else {
+//       newString = newString.concat(')');
+//     }
+//   });
 
-  return newString;
-}
+//   return newString;
+// }
 
-console.log(duplicateEncode('rEcede'));
+// console.log(duplicateEncode('rEcede'));
+
+// Question #125:Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string (alphabetical ascending), the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+// solution
+// function longest(s1, s2) {
+//   const combinedSet = new Set([...s1, ...s2]);
+//   return [...combinedSet].sort().join('');
+// }
+
+// console.log(longest('loopingisfunbutdangerous', 'lessdangerousthancoding'));
