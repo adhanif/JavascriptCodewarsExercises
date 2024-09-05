f6c73efbe8220061065c096c248e080bae134b9e
ad.hanif <ad.hanif@outlook.com>
2024-09-05 23:05:20 +0300

    Question #127 solved

--- index.js
+++ index.js
@@ -2659,3 +2659,19 @@
 // }
 
 // console.log(towerBuilder(5));
+
+// Question #127: Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates). Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+// Input >> Output Examples
+// minValue ({1, 3, 1})  ==> return (13)
+// Explanation:
+// (13) is the minimum number could be formed from {1, 3, 1} , Without duplications
+// minValue({5, 7, 5, 9, 7})  ==> return (579)
+// minValue({1, 9, 3, 1, 7, 4, 6, 6, 7}) return  ==> (134679)
+
+// solution
+// function minValue(values) {
+//   const mySet = new Set(values);
+//   const myArray = Array.from(mySet);
+//   return parseInt(myArray.sort().join(''));
+// }
+// console.log(minValue([1, 9, 3, 1, 7, 4, 6, 6, 7]));
