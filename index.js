ec2d82696e24eeb443c6de2532c803b382b3a850
ad.hanif <ad.hanif@outlook.com>
2025-01-18 18:57:24 +0200

    Question #131 solved

--- index.js
+++ index.js
@@ -2775,3 +2775,14 @@
 // };
 
 // '1a2b3c4d5E'.toAlternatingCase();
+
+// Question #131: Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even. For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples. The input will be a lowercase string with no spaces.
+
+// function capitalize(s) {
+//   const chars = [...s];
+//   return [
+//     chars.map((c, i) => (i % 2 === 0 ? c.toUpperCase() : c)).join(''),
+//     chars.map((c, i) => (i % 2 !== 0 ? c.toUpperCase() : c)).join(''),
+//   ];
+// }
+// capitalize('abcdef');
