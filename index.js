04ffe5613be556511f2c54275b15ca318fc2f281
ad.hanif <ad.hanif@outlook.com>
2025-01-21 22:18:26 +0200

    Question #132 solved

--- index.js
+++ index.js
@@ -2786,3 +2786,15 @@
 //   ];
 // }
 // capitalize('abcdef');
+
+// Question #132:  Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+// * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+// * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+// * url = "https://www.cnet.com"                -> domain name = cnet"
+
+// solution
+// function domainName(url) {
+//   return url.replace('http://', '').replace('https://', '').replace('www.', '').split('.')[0];
+// }
+
+// domainName('https://youtube.com');
