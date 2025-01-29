ea679cf28e35783f16a14bffce7514f23e8915ba
ad.hanif <ad.hanif@outlook.com>
2025-01-29 22:53:00 +0200

    Question #138 solved

--- index.js
+++ index.js
@@ -2930,3 +2930,17 @@
 //   }
 // }
 // twoSum([3, 2, 4], 6);
+
+//Question #138: Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array. You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2. The array will always contain letters in only one case.
+
+// Example:
+// ['a','b','c','d','f'] -> 'e'
+// ['O','Q','R','S'] -> 'P'
+
+// solution;
+// function findMissingLetter(array) {
+//   for (let i = 0; i < array.length - 1; i++) {
+//     if (array[i + 1].charCodeAt() - array[i].charCodeAt() > 1) return String.fromCharCode(array[i].charCodeAt() + 1);
+//   }
+// }
+// findMissingLetter(['a', 'b', 'c', 'd', 'f']);
