025c8e9051d7d3f99bf336343a49cd1116fef8aa
adhanif <ad.hanif@outlook.com>
2025-03-07 23:59:18 +0200

    Question #143 solved

--- index.js
+++ index.js
@@ -3021,3 +3021,23 @@
 // }
 
 // createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+
+// Question #143: Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
+// solutions
+// const addProperty = (questions) => {
+//   questions.forEach((q) => (q.usersAnswer = null));
+//   console.log(questions);
+// };
+
+// addProperty([
+//   {
+//     question: "What's the currency of the USA?",
+//     choices: ['US dollar', 'Ruble', 'Horses', 'Gold'],
+//     corAnswer: 0,
+//   },
+//   {
+//     question: 'Where was the American Declaration of Independence signed?',
+//     choices: ['Philadelphia', 'At the bottom', "Frankie's Pub", 'China'],
+//     corAnswer: 0,
+//   },
+// ]);
