7092e576a4faabcb8c369373f0d82a0c5482dbe1
adhanif <ad.hanif@outlook.com>
2025-03-16 22:28:54 +0200

    Question #145 solved

--- index.js
+++ index.js
@@ -3041,3 +3041,19 @@
 //     corAnswer: 0,
 //   },
 // ]);
+
+// Question #144:  Create a function named divisors/Divisors that takes an integer n > 1 and
+// returns an array with all of the integer's divisors(except for 1 and the number itself),
+// from smallest to largest. If the number is prime return the string '(integer) is prime'
+
+// function divisors(integer) {
+//   let divisors = [];
+//   for (let i = 2; i <= integer - 1; i++) {
+//     if (integer % i === 0) {
+//       divisors.push(i);
+//     }
+//   }
+//   return divisors.length === 0 ? `${integer} is prime` : divisors;
+// }
+
+// divisors(13);
