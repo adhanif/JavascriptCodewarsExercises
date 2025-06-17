99846275f8d1e45e1b6d7c1ab8cf73d9df9f21a3
adhanif <ad.hanif@outlook.com>
2025-06-17 23:23:50 +0300

    Question #150

--- index.js
+++ index.js
@@ -3190,3 +3190,37 @@
 // };
 
 // console.log(mostFrequestFinder('javassscript'));
+
+// Question #150: Sentence Calculator
+// Calculate the total score (sum of the individual character scores) of a sentence given
+// the following score rules for each allowed group of characters:
+
+// Lower case [a-z]: 'a'=1, 'b'=2, 'c'=3, ..., 'z'=26
+// Upper case [A-Z]: 'A'=2, 'B'=4, 'C'=6, ..., 'Z'=52
+// Digits [0-9] their numeric value: '0'=0, '1'=1, '2'=2, ..., '9'=9
+// Other characters: 0 value
+// Note: input will always be a string
+
+// solution:
+// function lettersToNumbers(s) {
+//   s = s.split(' ').join('');
+//   let all = {};
+//   [...'abcdefghijklmnopqrstuvwxyz'].forEach((a, i) => {
+//     all[a] = i + 1;
+//   });
+//   [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'].forEach((a, i) => {
+//     all[a] = i * 2 + 2;
+//   });
+//   [...'0123456789'].forEach((a, i) => {
+//     all[a] = i;
+//   });
+//   let total = 0;
+
+//   s.split('').forEach((a) => {
+//     if (Object.hasOwn(all, a)) {
+//       total += all[a];
+//     }
+//   });
+//   return total;
+// }
+// console.log(lettersToNumbers('Give me five!'));
