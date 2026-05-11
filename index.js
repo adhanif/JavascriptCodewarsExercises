bcf0c78b505e38e263f0a609caf2bae4adb301ba
adhanif <ad.hanif@outlook.com>
2026-05-11 21:25:34 +0300

    Question #155 solved

--- index.js
+++ index.js
@@ -3331,7 +3331,6 @@
 
 // console.log(maskify('1232222lweh89745873!!!'));
 
-
 // Question #154: Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
 // Examples:
 // 1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
@@ -3344,3 +3343,32 @@
 //   return n % x === 0 && n % y === 0 ? true : false;
 // }
 // console.log(isDivisible(3, 3, 4));;
+
+
+
+// Question #155:Given an array/list of integers, find the maximum sum of 3 DISTINCT array elements.
+
+// Notes :
+// array size is at least 3 .
+// array elements can be zero or negative
+// Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
+// Input >> Output Examples
+// maxTriSum ({3,2,6,8,2,3}) ==> return (17)
+// Best triplet = {6,8,3}, its sum is 17
+// maxTriSum ({2,1,8,0,6,4,8,6,2,4}) ==> return (18)
+// Best triplet = {8, 6, 4} , its sum is 18.
+// maxTriSum ({-7,12,-7,29,-5,0,-7,0,0,29}) ==> return (41)
+// Best triplet = {12 , 29 , 0} , its sum is 41
+
+const maxTrimSum = (num) => {
+  const set = new Set(num);
+  num.forEach((n, i) => {
+    set.add(n);
+  });
+  while(set.size>3){
+    set.delete(Math.min(...set));
+  }
+  return [...set].reduce((acc, curr) => acc + curr, 0);
+};
+
+console.log(maxTrimSum([2, 1, 8, 0, 6, 4, 8, 6, 2, 4]));
